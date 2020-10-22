673b649226315f463a268f4ec7980ff0956b0bd5
mikhail <mihail_kotov_86@mail.ru>
2020-10-22 15:37:53 +0300

    Module 3 Tasks 3,4

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,25 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+
+  if (str === '') {
+    return '';
+  }
+
+  let strToArr = str.split('');
+  let newArr = [];
+
+  for (let i = 0; i < strToArr.length; i++) {
+
+    if (strToArr[i] === '-') {
+      let upLetter = strToArr[i + 1].toUpperCase();
+      newArr.push(upLetter);
+      i++;
+      continue;
+    }
+    newArr.push(strToArr[i]);
+  }
+
+  return newArr.join('')
+
 }
