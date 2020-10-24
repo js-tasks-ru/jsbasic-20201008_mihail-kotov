6e069e9d787578d671a0b3cf35005e4b2170b875
mikhail <mihail_kotov_86@mail.ru>
2020-10-24 16:25:26 +0300

    Module 4 Task 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,19 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+
+  let rows = table.rows;
+  let counter = 0;
+
+  for (let i = 0; i < rows.length; i++) {
+    let cells = rows[i].cells;
+
+    for (let j = 0; j < cells.length; j++) {
+      console.log(cells[j]);
+      cells[counter].style.backgroundColor = 'red';
+    }
+
+    counter++;
+
+  }
 }
