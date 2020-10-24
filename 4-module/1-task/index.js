5e8227163475d1e4e7b4a26389c55facc957b849
mikhail <mihail_kotov_86@mail.ru>
2020-10-24 15:58:59 +0300

    Module 4 Task 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let ul = document.createElement('ul');
+
+  for (let item of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = `${item.firstName} ${item.lastName}`;
+    ul.insertAdjacentElement('afterbegin', li);
+  }
+
+  return ul;
 }
