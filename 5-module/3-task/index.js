038d0f1574e1ca00610c8e8f9a447fb3895e3415
mikhail <mihail_kotov_86@mail.ru>
2020-10-26 19:31:51 +0300

    Module 5 Tasks 1,2,3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,47 @@
 function initCarousel() {
-  // ваш код...
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let btnRight = document.querySelector('.carousel__arrow_right');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselSlides = document.querySelectorAll('.carousel__slide');
+
+  let carouselInnerWidth = carouselInner.offsetWidth;
+  let counter = 0;
+
+  let maxWidth = (carouselSlides.length - 1) * -carouselInnerWidth;
+  let minWidth = 0;
+
+  if (counter === minWidth) {
+    btnLeft.style.display = 'none';
+  } else {
+    btnLeft.style.display = '';
+  }
+
+  carouselInner.style.transform = `translateX(-${counter}px)`;
+
+  btnRight.addEventListener('click', (evt) => {
+
+    counter -= carouselInnerWidth;
+
+    if (counter === maxWidth) {
+      btnRight.style.display = 'none';
+      btnLeft.style.display = '';
+    } else {
+      btnRight.style.display = '';
+    }
+
+    carouselInner.style.transform = `translateX(${counter}px)`;
+  });
+
+  btnLeft.addEventListener('click', (evt) => {
+    counter += carouselInnerWidth;
+
+    if (counter === minWidth) {
+      btnLeft.style.display = 'none';
+      btnRight.style.display = '';
+    } else {
+      btnLeft.style.display = '';
+    }
+
+    carouselInner.style.transform = `translateX(${counter}px)`;
+  });
 }
