421c7bd36760b2e9c514e1a9bf72312737c51bf4
mikhail <mihail_kotov_86@mail.ru>
2020-12-01 07:31:52 +0300

    Module 7 and 8

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -5,6 +5,7 @@ export default class CartIcon {
     this.render();
 
     this.addEventListeners();
+    this.initialTopCoord = null;
   }
 
   render() {
@@ -21,9 +22,11 @@ export default class CartIcon {
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
 
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
       this.updatePosition();
 
       this.elem.classList.add('shake');
+
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
       }, {once: true});
@@ -39,6 +42,51 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    let cart = this.elem;
+    let cartIsHidden = this.isHidden(cart);
+
+    if (cartIsHidden) {
+      return;
+    }
+
+    let isMobile = document.documentElement.clientWidth <= 767;
+
+  // Если условие выполняется, обнуляем стили к исходным
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    } else {
+      if (window.pageYOffset > this.initialTopCoord) {
+        let leftIndent = Math.min(
+          document.querySelector('.container').getBoundingClientRect().right + 20,
+          document.documentElement.clientWidth - this.elem.offsetWidth - 10
+        ) + 'px'
+
+        Object.assign(this.elem.style, {
+          position: 'fixed',
+          top: '50px',
+          zIndex: 1e3,
+          right: '10px',
+          left: leftIndent
+        });
+
+      } else {
+        Object.assign(this.elem.style, {
+          position: '',
+          top: '',
+          left: '',
+          zIndex: ''
+        })
+      }
+    }
   }
+
+  isHidden(elem) {
+    return !elem.offsetWidth && !elem.offsetHeight;
+  }
+
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,36 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = document.createElement('div');
+    this.elem.classList.add('products-grid');
+    this.elem.insertAdjacentHTML('afterbegin', `<div class='products-grid__inner'></div>`);
+    this.render(products);
+  }
+
+  render(data) {
+    for (let i = 0; i < data.length; i++) {
+      let gridInnerContent = new ProductCard(data[i]).elem;
+
+      this.elem.querySelector('.products-grid__inner').append(gridInnerContent);
+    }
+  }
+
+  updateFilter(filters) {
+    this.filters = Object.assign(this.filters, filters)
+
+    let noNuts = this.filters.noNuts;
+    let vegeterianOnly = this.filters.vegeterianOnly;
+    let maxSpiciness = this.filters.maxSpiciness;
+    let category = this.filters.category;
+
+    let result = this.products.filter((item) =>
+      (!noNuts || item.nuts !== true) &&
+      (!vegeterianOnly || item.vegeterian  === true) &&
+      (maxSpiciness === undefined || item.spiciness  <= maxSpiciness) &&
+      (!category || category === '' || item.category === category)
+    );
+
+    this.elem.querySelector('.products-grid__inner').innerHTML = '';
+    this.render(result);
   }
 }
