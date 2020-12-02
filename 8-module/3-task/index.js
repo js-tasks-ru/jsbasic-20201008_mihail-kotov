fd290912ddeea93792ee24322832a51d8ea89031
mikhail <mihail_kotov_86@mail.ru>
2020-12-02 19:28:22 +0300

    Module 8 task 3

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,28 +6,65 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    let hasCartItem = this.cartItems.find(item => item.product.id === product.id);
+
+    if (hasCartItem === undefined) {
+      let cartItem = {};
+      cartItem.product = product;
+      cartItem.count = 1;
+      this.cartItems.push(cartItem);
+      this.onProductUpdate(cartItem);
+    }
+
+    if (hasCartItem !== undefined) {
+      let cartItemCountIncrease = this.cartItems.find(item => item.product.id === product.id);
+      cartItemCountIncrease.count = cartItemCountIncrease.count + 1;
+      this.onProductUpdate(cartItemCountIncrease);
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    if (this.cartItems.length === 0) {
+      return
+    }
+
+    if (amount === 1) {
+      let cartItemCountIncrease = this.cartItems.find(item => item.product.id === productId);
+      cartItemCountIncrease.count = cartItemCountIncrease.count + 1;
+      this.onProductUpdate(cartItemCountIncrease);
+    }
+
+    if (amount === -1) {
+      let cartItemCountDecrease = this.cartItems.find(item => item.product.id === productId);
+      cartItemCountDecrease.count = cartItemCountDecrease.count - 1;
+
+      if (cartItemCountDecrease.count === 0) {
+        let index = this.cartItems.findIndex(item => item.product.id === productId);
+        this.cartItems.splice(index, 1)
+        this.onProductUpdate(cartItemCountDecrease);
+      }
+
+      this.onProductUpdate(cartItemCountDecrease);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0 ? true : false
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((acc, currentItem) => {
+      return acc + currentItem.count;
+    }, 0)
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((acc, currentItem) => {
+      return acc + currentItem.product.price * currentItem.count;
+    }, 0)
   }
 
   onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
